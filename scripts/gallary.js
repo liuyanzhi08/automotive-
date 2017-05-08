7c5253fea0bf7733618f8f375b3d947752d2d7b4
liuyanzhi08 <702368372@qq.com>
2017-05-08 20:50:16 +0800

    first commit

---
+++ scripts/gallary.js
@@ -0,0 +1,24 @@
+;(function () {
+    $('[component-gallary]').each(function (index, rootDiv) {
+        var $root = $(rootDiv);
+        var $container = $root.find('.gallary-imgs');
+        var $navLeft = $root.find('.gallary-nav-left');
+        var $navRight = $root.find('.gallary-nav-right');
+        var containerWidth = $container.width();
+        var itemWidth = $container.find('li').outerWidth(true);
+        var maxNum = Math.floor(containerWidth / itemWidth);
+        var totalNum = $container.find('li').length;
+        var totalWidth = totalNum * itemWidth;
+        var perViewWidth = itemWidth * maxNum;
+        $container.find('ul').css('width', totalWidth);
+
+        var delay = 300;
+        $navLeft.click(function () {
+            $container.animate({'scrollLeft': '-=' + perViewWidth}, delay);
+        })
+        $navRight.click(function () {
+            $container.animate({'scrollLeft': '+=' + perViewWidth}, delay);
+        })
+    })
+
+} ());
